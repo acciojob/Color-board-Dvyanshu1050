e3b6b1cc236cb2a487cab9117a9a810e098291af
DIVYANSHU KUMAR MAURYA <155415613+Dvyanshu1050@users.noreply.github.com>
2026-02-06 10:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const container = document.getElementById("container");
+const SQUARES = 800;
+
+const colors = [
+  "#e74c3c",
+  "#8e44ad",
+  "#3498db",
+  "#e67e22",
+  "#2ecc71",
+  "#f1c40f",
+];
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  setTimeout(() => {
+    element.style.backgroundColor = "#1d1d1d";
+  }, 1000);
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
